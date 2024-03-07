c8129e8316a363622279a11da69a4c4d5460eb19
Sepideh Attar <sepideh.att@gmail.com>
2024-03-07 21:56:00 +0100

    Added getEpisodes api to apiService

--- src/Services/ApiServices.jsx
+++ src/Services/ApiServices.jsx
@@ -17,7 +17,16 @@ export class ApiServices {
 
     async getEpisodes(){
       try {
-        const response = await this.httpClient.get(`${baseUrl}/episode`)
+        const response = await this.httpClient.get()
+        return response.json();
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    async getEpisode(url){
+      try {
+        const response = await this.httpClient.get(url)
         return response.json();
       } catch (error) {
         console.log(error)
