3017df2568ea9c1543e49ea7a31e7bdcf245a59d
Sepideh Attar <sepideh.att@gmail.com>
2024-03-08 18:39:15 +0100

    Removed fetch episodes per characters from character detail page

--- src/Components/CharacterDetail.jsx
+++ src/Components/CharacterDetail.jsx
@@ -2,32 +2,9 @@
 import { ExpendableButton } from './ExpandableButton';
 import useOpenController from "../Hooks/useOpenController";
 import NestedDetailTable from "./NestedDetailTable";
-import { useEffect, useState } from 'react';
-import { ApiServices } from '../Services/ApiServices';
-import { FetchClient } from '../ServiceClients/FetchClients';
 
 const CharacterDetail = ({character}) => {
   const { isOpen, toggle } = useOpenController (false);
-
-  const [episodes, setEpisodes] = useState([])
-
-  useEffect(() => {
-    const apiServices = new ApiServices(FetchClient)
-    const fetchData =async() =>{
-      try {
-        const result = await Promise.all(
-          character.episode.map(async(url) => {
-             return await apiServices.getEpisode(url);
-          })
-        )
-         setEpisodes(result)
-      } catch (error) {
-        console.error("Error fetching episodes:", error);
-      }
-    } 
-    fetchData()
-
-  }, [character, character.episode])
 
   return (
     <tbody>
@@ -44,7 +21,7 @@ const CharacterDetail = ({character}) => {
         {character.episode?.length == 51 ? `${character.episode?.length} (All Episodes)` : character.episode?.length }
         </td>
       </tr>
-      {isOpen && episodes && <NestedDetailTable character={character} episodes={episodes} />}
+      {isOpen && <NestedDetailTable character={character}  />}
       </tbody>
   )
 }
