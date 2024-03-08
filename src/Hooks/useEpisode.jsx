e7b5e23574aaf53b234693d6101f4b2977dfecb8
Sepideh Attar <sepideh.att@gmail.com>
2024-03-08 18:41:14 +0100

    Added useEpisode custom hook to fetch episods / added lazy load functionallity

---
+++ src/Hooks/useEpisode.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, useCallback } from 'react';
+import { ApiServices } from '../Services/ApiServices';
+import { FetchClient } from '../ServiceClients/FetchClients';
+
+const apiServices = new ApiServices(FetchClient);
+
+const useEpisodes = (episodeUrls) => {
+    const [episodes, setEpisodes] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    // Initialize with 10 or the total number of episodes if fewer than 10
+    const initialLoadAmount = Math.min(10, episodeUrls.length);
+    const [loadAmount, setLoadAmount] = useState(initialLoadAmount);
+
+    useEffect(() => {
+        // Immediate IIFE to load initial or additional episodes
+        (async () => {
+            if (!episodeUrls.length || loadAmount === 0) return;
+
+            setLoading(true);
+            setError(null);
+
+            try {
+                const urlsToLoad = episodeUrls.slice(0, loadAmount);
+                const result = await Promise.all(urlsToLoad.map(url => apiServices.getEpisode(url)));
+                setEpisodes(result);
+            } catch (error) {
+                setError(error);
+                console.error("Error fetching episodes:", error);
+            } finally {
+                setLoading(false);
+            }
+        })();
+    }, [episodeUrls, loadAmount]);
+
+    const loadMore = useCallback(() => {
+        // Only increase loadAmount if there are more episodes to load
+        if (loadAmount < episodeUrls.length) {
+            const nextLoadAmount = Math.min(loadAmount + 10, episodeUrls.length);
+            setLoadAmount(nextLoadAmount);
+        }
+    }, [loadAmount, episodeUrls.length]);
+
+    return { episodes, loading, error, loadMore };
+};
+export default useEpisodes
